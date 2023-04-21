45b1df69d30982893b693da3a526429fd8a4acbb
Rachel <thanhngocbach0904@gmail.com>
2023-04-21 22:34:46 +1000

    updated loading component style

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,23 @@
-import React from "react";
+// import React from "react";
+
+// const Loading = () => {
+//   return (
+//     <section className="section">
+//       <h1 className="section-title"> Loading ....</h1>
+//     </section>
+//   );
+// };
+
+// export default Loading;
 
 const Loading = () => {
   return (
-    <section className="section">
-      <h1 className="section-title"> Loading ....</h1>
-    </section>
+    <div className="d-flex justify-content-center text-primary" style={{ marginTop: "4rem" }}>
+      <div className="spinner-border" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
   );
-};
+}
 
 export default Loading;
